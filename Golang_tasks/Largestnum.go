54840e15dc16519e31992c433e2b06c117a63180
“vighneshpol” <vighnesh.pol@pantheon.io>
2022-08-23 14:18:07 +0530

    updated

---
+++ Golang_tasks/Largestnum.go
@@ -0,0 +1,27 @@
+/*
+Write a program to find largest number from given 3 number.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+
+	var number1, number2, number3 int
+
+	fmt.Print("\nEnter the Three Numbers to find Largest = ")
+	fmt.Scanln(&number1)
+	fmt.Scanln(&number2)
+	fmt.Scanln(&number3)
+
+	if number1 > number2 && number1 > number3 {
+		fmt.Println(number1, "is large than ", number2, " and ", number3)
+	} else if number2 > number1 && number2 > number3 {
+		fmt.Println(number2, "is large than", number1, " and ", number3)
+	} else if number3 > number1 && number3 > number2 {
+		fmt.Println(number3, " is large tha  ", number1, " and ", number2)
+	} else {
+		fmt.Println("All of ")
+	}
+}
